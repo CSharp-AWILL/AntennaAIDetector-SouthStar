1101db5d221104a22a327c980499bcab881fe8fc
xiawuhao2013 <xiawuhao2011@foxmail.com>
2019-12-16 10:23:03 +0800

    feat: add DataSave

---
+++ AntennaAIDetector-SouthStar/DataSave/DataSaveForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AntennaAIDetector_SouthStar.DataSave
+{
+    public partial class DataSaveForm : Form
+    {
+        public DataSaveForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
