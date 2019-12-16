b5e03020c1dafcb102474f3b0eb6fd564b48bdfe
xiawuhao2013 <xiawuhao2011@foxmail.com>
2019-12-16 11:04:19 +0800

    feat(DataSave): build the core routine of DataSave process

--- AntennaAIDetector-SouthStar/DataSave/DataSave.cs
+++ AntennaAIDetector-SouthStar/DataSave/DataSave.cs
@@ -3,10 +3,90 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AntennaAIDetector_SouthStar.Product;
+using Aqrose.Framework.Utility.MessageManager;
 
 namespace AntennaAIDetector_SouthStar.DataSave
 {
-    class DataSave
+    public class DataSave
     {
+        private string TimeInfoOfLast = "";
+        private string TimeInfoOfCurr = "";
+
+        public string DirectoryPath { get; set; } = "";
+        public int SpanOfTime { get; set; } = 10;
+        public int QueueSize { get; set; } = 300;
+        public Queue<ResultData> ResultDatas { get; private set; } = new Queue<ResultData>();
+
+        public DataSave()
+        {
+        }
+
+        private void WriteCsv()
+        {
+            var resultData = GetCurrResultData();
+
+            if (null == resultData)
+            {
+                MessageManager.Instance().Info("DataSave.WriteCsv: null resultData.");
+
+                return;
+            }
+
+            if (CanGenerateNewCsvFile())
+            {
+                MessageManager.Instance().Info("DataSave.WriteCsv: generate new csv file.");
+                // TODO: generate new csv file
+
+            }
+
+            // TODO: write csv
+
+            return;
+        }
+
+        private void EnqueueResultDatas()
+        {
+            // TODO: confused part--when?
+            ResultData resultData = new ResultData();
+
+
+            if (null != ResultDatas && QueueSize <= ResultDatas.Count)
+            {
+                ResultDatas.Dequeue();
+            }
+            ResultDatas.Enqueue(resultData);
+
+            return;
+        }
+
+        private string GetTimeInfo()
+        {
+            string timeInfo = "";
+            // TODO: get current time info
+
+            return timeInfo;
+        }
+
+        private bool CanGenerateNewCsvFile()
+        {
+            // TODO: judge whether generate new csv file or not
+
+            return false;
+        }
+
+        private ResultData GetCurrResultData()
+        {
+            if (null == ResultDatas || 0 > ResultDatas.Count)
+            {
+                MessageManager.Instance().Info("DataSave.GetCurrResultData: empty results queue.");
+
+                return null;
+            }
+
+            return ResultDatas.Peek();
+        }
+
+        
     }
 }
--- AntennaAIDetector-SouthStar/Product/ResultData.cs
+++ AntennaAIDetector-SouthStar/Product/ResultData.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace AntennaAIDetector_SouthStar.Product
 {
-    class ResultData
+    public class ResultData
     {
     }
 }
