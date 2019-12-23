3c5141b5754b2f875b678a1c9b39f40aa330d6e6
xiawuhao2013 <xiawuhao2011@foxmail.com>
2019-12-23 11:41:15 +0800

    feat: add DisplayChar

---
+++ AntennaAIDetector-SouthStar/Result/DisplayChar.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AqVision.Graphic.AqVision.shape;
+
+namespace AntennaAIDetector_SouthStar.Result
+{
+    public class DisplayChar
+    {
+        public string Text { get; set; } = "";
+        public Point Position { get; set; } = new Point();
+        public Size Size { get; set; } = new Size();
+        public Color Color { get; set; } = new Color();
+
+        public DisplayChar()
+        {
+        }
+
+        public AqCharacter ConvertToAqCharacter()
+        {
+            AqCharacter aqCharacter = new AqCharacter();
+
+            aqCharacter.AqString = Text;
+            aqCharacter.LeftTopPointX = Position.X;
+            aqCharacter.LeftTopPointY = Position.Y;
+            aqCharacter.RightBottomPointX = Position.X + Size.Width;
+            aqCharacter.RightBottomPointY = Position.Y + Size.Height;
+            aqCharacter.ColorRed = Convert.ToInt32(Color.R);
+            aqCharacter.ColorGreen = Convert.ToInt32(Color.G);
+            aqCharacter.ColorBlue = Convert.ToInt32(Color.B);
+
+            aqCharacter.isDragCharacter = true;
+            aqCharacter.isVisible = false;
+
+            return aqCharacter;
+        }
+    }
+}
--- AntennaAIDetector-SouthStar/Result/SingleResult.cs
+++ AntennaAIDetector-SouthStar/Result/SingleResult.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace AntennaAIDetector_SouthStar.Result
     {
         public int Index { get; private set; } = 0;
         public string DefectInfo { get; private set; } = "";
+        public DisplayChar DisplayChar { get; private set; } = new DisplayChar();
 
         private SingleResult()
         {
@@ -19,6 +21,14 @@ namespace AntennaAIDetector_SouthStar.Result
         {
             Index = index;
             DefectInfo = defectInfo;
+        }
+
+        public void SetDisplayChar(int heightOfImage)
+        {
+            DisplayChar.Text = DefectInfo;
+            DisplayChar.Position = new Point(10, heightOfImage * Index + 10);
+            DisplayChar.Size = new Size(200, 200);
+            DisplayChar.Color = Color.Red;
         }
     }
 }
